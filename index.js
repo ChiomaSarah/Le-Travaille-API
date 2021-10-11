b10481eb6370d4bbb14c22c966b9f60a6d7f0fc2
CHIOMA SARAH OSUJI <78868674+ChiomaSarah@users.noreply.github.com>
2021-10-11 06:58:44 +0100

    heroku database

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const pool = require("./db");
 const app = express();
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+
 app.use(cors("*"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
